bb62490e697eb1ce9bb865cd527161eb4c73e43f
dy <2657179843@qq.com>
2020-06-23 14:09:42 +0800

    new page

---
+++ src/views/404.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="state-404 flex-center">
+    <h2>404页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {},
+}
+</script>
+<style lang="less" scoped>
+.state-404 {
+}
+</style>
---
+++ src/views/login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="login flex-center">
+    <h2>登陆页面</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {},
+}
+</script>
+<style lang="less" scoped>
+.login {
+}
+</style>
